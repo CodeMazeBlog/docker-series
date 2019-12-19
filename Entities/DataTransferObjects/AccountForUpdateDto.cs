7815112706aa47cfc6944999d6d52c4ad20ea7c6
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2019-12-19 23:26:54 +0100

    Revert "Revert "Project update and .NET Core upgraded to 3.1""
    This reverts commit 6f9c4145883d7ac62e572e7c36b92c2c6ae2b1bc.

---
+++ Entities/DataTransferObjects/AccountForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.DataTransferObjects
+{
+	public class AccountForUpdateDto
+	{
+		public DateTime DateCreated { get; set; }
+		public string AccountType { get; set; }
+	}
+}
